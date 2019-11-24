a87b8535ab857e1ca9471c6283d9cb55f14b8705
Aastha Goel <aastha.goel@cygrp.com>
2019-11-25 00:22:06 +0530

    Interviewer CRUD complete

--- ms-hr/src/models/interviewer.model.js
+++ ms-hr/src/models/interviewer.model.js
@@ -1,22 +1,63 @@
 const { BaseModel } = require("@the-medicsoft/webapi-framework");
 const { model } = require("mongoose");
 
-const { VacancySchema } = require("../schemas");
+const { InterviewerSchema } = require("../schemas");
 
-class VacancyModel extends BaseModel {
+class InterviewerModel extends BaseModel {
   constructor() {
-    super(model("Vacancy", VacancySchema));
+    super(model("Interviewer", InterviewerSchema));
   }
 
-  async getVacancies() {
-    const vacancies = await super.read();
+  async getInterviewers() {
+    const interviewers = await super.read();
 
-    if (vacancies) {
-      return super.success({ total: vacancies.length, data: vacancies, message: '' });
+    if (interviewers) {
+      return super.success({
+        total: interviewers.length,
+        data: interviewers,
+        message: ""
+      });
     } else {
       super.notFound();
     }
   }
+
+  async createInterviewer({ body }) {
+    console.log(body);
+    const response = await super.create({ body });
+
+    if (response) {
+      return super.success({
+        message: "Interviewer Added"
+      });
+    } else {
+      super.fail({ message: "Error: Interviewer could not be added" });
+    }
+  }
+
+  async updateInterviewer({ id, body }) {
+    const response = await super.update({ id, body });
+
+    if (response) {
+      return super.success({
+        message: "Interviewer Updated"
+      });
+    } else {
+      super.notFound({ message: "Interviewer Not Found" });
+    }
+  }
+
+  async deleteInterviewer({ id, useSoftDelete, deleteDoc }) {
+    const response = await super.delete({ id, useSoftDelete });
+
+    if (response) {
+      return super.success({
+        message: "Interviewer Deleted"
+      });
+    } else {
+      super.notFound({ message: "Interviewer Not Deleted" });
+    }
+  }
 }
 
-module.exports = { VacancyModel };
+module.exports = { InterviewerModel };
