6337b5644ee85fd5990ccf3f51a71b24e89e52bd
Amandeep Rikhi <amandeeprikhi@gmail.com>
2019-11-26 22:24:14 +0530

    adding components

--- ms-ui/src/app/app.module.ts
+++ ms-ui/src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { VacancyComponent } from "./vacancy/vacancy.component";
+import { InterviewerComponent } from "./interviewer/interviewer.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
+import { MatGridListModule } from "@angular/material/grid-list";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    VacancyComponent,
+    InterviewerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
