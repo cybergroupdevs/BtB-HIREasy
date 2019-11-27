baf9162625f39509266d1d92cab7e5a08cd0536e
rakshan1996 <rakshanthapar32@gmail.com>
2019-11-27 19:41:20 +0530

    Updated Routes and Controller

--- ms-email/src/models/email.model.js
+++ ms-email/src/models/email.model.js
@@ -1,15 +1,17 @@
 const { BaseModel } = require("@the-medicsoft/webapi-framework");
 const { model } = require("mongoose");
 
-const { EmailSchema } = require("../schemas");
+const { selectedCandidate } = require("../schemas");
+
+const consts = require('../helpers/constants.json');
 
 class EmailModel extends BaseModel {
   constructor() {
-    super(model("Email", EmailSchema));
+    super(model("selectedCandidate", selectedCandidate));
   }
 
-  async getEmails() {
-    const emails = await super.read();
+  async getEmails(Id) {
+    const emails = await super.readByQuery({ query: { vacancyId: Id } });
 
     if (emails.length) {
       return super.success({
@@ -22,41 +24,18 @@ class EmailModel extends BaseModel {
     }
   }
 
-  async createVacancy({ body }) {
+  async sendEmails({ body }) {
     const response = await super.create({ body });
 
     if (response) {
       return super.success({
-        message: "Vacancy Posted"
+        message: consts.email.success
       });
     } else {
       super.fail({ message: "Error: Vacancy Not Posted" });
     }
   }
 
-  async updateVacancy({ id, body }) {
-    const response = await super.update({ id, body });
-
-    if (response) {
-      return super.success({
-        message: "Vacancy Updated"
-      });
-    } else {
-      super.notFound({ message: "Vacancy Not Found" });
-    }
-  }
-
-  async deleteVacancy({ id, useSoftDelete, deleteDoc }) {
-    const response = await super.delete({ id, useSoftDelete });
-
-    if (response) {
-      return super.success({
-        message: "Vacancy Deleted"
-      });
-    } else {
-      super.notFound({ message: "Vacancy Not Deleted" });
-    }
-  }
 }
 
 module.exports = { EmailModel };
