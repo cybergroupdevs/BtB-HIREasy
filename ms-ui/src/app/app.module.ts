eb7a02b65ecf35a01c339f3730c2cc8696c1d194
Amandeep Rikhi <amandeeprikhi@gmail.com>
2019-12-01 21:45:11 +0530

    - Added ChartsJS - Added Material Tables

--- ms-ui/src/app/app.module.ts
+++ ms-ui/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -8,9 +9,11 @@ import { VacancyComponent } from "./vacancy/vacancy.component";
 import { InterviewerComponent } from "./interviewer/interviewer.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
+import { ChartsModule } from "ng2-charts";
+
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatCardModule } from "@angular/material/card";
-import { MatGridListModule } from "@angular/material/grid-list";
+import { MatTableModule } from "@angular/material/table";
 
 @NgModule({
   declarations: [
@@ -22,10 +25,12 @@ import { MatGridListModule } from "@angular/material/grid-list";
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatCardModule,
-    MatGridListModule
+    MatTableModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ms-ui/src/app/interviewer/interviewer.component.ts
+++ ms-ui/src/app/interviewer/interviewer.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-interviewer',
-  templateUrl: './interviewer.component.html',
-  styleUrls: ['./interviewer.component.css']
+  selector: "app-interviewer",
+  templateUrl: "./interviewer.component.html",
+  styleUrls: ["./interviewer.component.css"]
 })
 export class InterviewerComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
