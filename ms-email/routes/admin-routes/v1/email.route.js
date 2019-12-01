7374d4f09fb7eb7cc6428b0522c334a2b35d94de
rakshan1996 <rakshanthapar32@gmail.com>
2019-12-02 01:41:08 +0530

    Initial Set up For NodeMailer Complete. Api Integration With Node Mailer InProgress

--- ms-email/routes/admin-routes/v1/email.route.js
+++ ms-email/routes/admin-routes/v1/email.route.js
@@ -3,7 +3,7 @@ const { EmailController } = require("../../../src/controllers");
 module.exports = (fastify, option, done) => {
   const email = new EmailController();
 
-  fastify.get("/email", email.getCandidates);
+  //fastify.get("/email", email.getCandidates);
 
   fastify.post("/email", email.sendEmail);
 
