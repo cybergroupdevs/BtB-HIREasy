b356830d7dbd1ae6ff2418f1b8d1fa1cc8f6fe0e
rakshan1996 <rakshanthapar32@gmail.com>
2019-12-01 21:43:01 +0530

    Auto Increment candidateId ( Work in Progress)

--- ms-email/src/schemas/email.schema.js
+++ ms-email/src/schemas/email.schema.js
@@ -1,12 +1,21 @@
-const { Schema } = require("mongoose");
+const mongoose =require('mongoose');
+const { Schema } = mongoose;
+const autoIncrement = require('mongoose-sequence')(mongoose);
 
-exports.selectedCandidate = new Schema([{
+const selectedCandidate = new Schema([{
     vacancyId: Number,
     candidate: [{
         emailAddress: String,
         fullName: String,
         phone: String,
         isSent: { type: Boolean, default: false },
+        candidateId:{type:Number},
         _id: false
     }]
 }]);
+selectedCandidate.plugin(autoIncrement,{
+    inc_field: 'candidate.candidateId',
+    start_seq:101
+});
+
+module.exports = {selectedCandidate};
