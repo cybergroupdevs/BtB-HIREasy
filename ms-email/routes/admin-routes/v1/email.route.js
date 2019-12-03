bbf6215e718339a840963152aa550305cfb013ad
rakshan1996 <rakshanthapar32@gmail.com>
2019-12-03 19:38:13 +0530

    Removed Committed Code and modified routes

--- ms-email/routes/admin-routes/v1/email.route.js
+++ ms-email/routes/admin-routes/v1/email.route.js
@@ -3,9 +3,7 @@ const { EmailController } = require("../../../src/controllers");
 module.exports = (fastify, option, done) => {
   const email = new EmailController();
 
-  //fastify.get("/email", email.getCandidates);
-
-  fastify.post("/email", email.sendEmail);
+  fastify.post("/sendEmails", email.sendEmail);
 
   done();
 };
